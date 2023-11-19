75c8944b7ab2d6ef5bffa2ebe9d3e4b1d5bf3934
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-19 15:21:26 +0200

    form logic

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,36 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    search: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.search.toLowerCase().trim());
+    this.setState({ search: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          onChange={this.handleChange}
+          value={this.state.search}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,51 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    total: 0,
+    error: null,
+    loader: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query: prevQuery, page: prevPage } = prevState;
+    const { query, page } = this.state;
+    if (prevQuery !== query || prevPage !== page) {
+      this.getPhotos(query, page);
+    }
+  }
+
+  getPhotos = async (query, page) => {
+    this.setState({ loader: true });
+    try {
+      const { photos, total_results } = await ImageService.getImages(
+        query,
+        page
+      );
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        total: total_results,
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ loader: false });
+    }
+  };
+
+  handleSubmit = value => {
+    this.setState({ query: value });
+  };
+
   render() {
+    console.log(this.state.images);
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.handleSubmit} />
+        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
       </>
     );
   }
